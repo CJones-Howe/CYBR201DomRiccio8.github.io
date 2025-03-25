b564430a0c312d99767f1f182e52a03ade0a11fd
DomRiccio8 <driccio@albany.edu>
2025-03-24 20:06:03 -0400

    Add files via upload
    Lab for this weeks class

---
+++ Variables.js
@@ -0,0 +1,15 @@
+//using let
+let name = "Domenic";
+let age = "19";
+let isStudent = "True";
+
+document.getElementById("demo1").innerHTML =
+"<br>Name: " + name + "<br>Age: " + age + "<br>Student: " + isStudent;
+
+let favColor = "Purple";
+let favFood = "Sushi";
+let favSport = "Basketball";
+
+document.getElementById("demo2").innerHTML =
+"<br>Favorite Color: " + favColor + "<br>Favorite Food: " + favFood + "<br>Favorite Sport: " +favSport;
+
---
+++ output.js
@@ -0,0 +1,13 @@
+function showInnerHTML() {
+    document.getElementById('output1').innerHTML = "Hello from inner HTML!";
+}
+
+function useDocumentWrite(){
+    document.write("This replaces the page content! Not recommended.");
+}
+function showAlert(){
+    alert("This is an alert box!");
+}
+function showConsole(){
+    console.log("This message is in the console.");
+}
