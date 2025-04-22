d82dd836ef42dd22cffeaa111b00c1eaaf684bc9
DomRiccio8 <driccio@albany.edu>
2025-04-22 12:50:28 -0400

    Add files via upload
    LeBron project.html is the main file for the website and all others are redirects

---
+++ LeBronp.js
@@ -0,0 +1,22 @@
+document.addEventListener("DOMContentLoaded", function() {
+document.getElementById("btn-ST.V").addEventListener("click", function(){
+    window.location.href = "ST.V ST.M.html"});
+
+
+document.getElementById("btn-OCC").addEventListener("click", function(){ 
+    window.location.href = "OCC.html"});
+
+
+document.getElementById("btn-MH").addEventListener("click", function(){
+    window.location.href = "MH.html"});
+
+
+document.getElementById("btn-CC").addEventListener("click", function(){
+    window.location.href = "CC.html"});
+
+
+document.getElementById("btn-LAL").addEventListener("click", function(){ 
+    window.location.href = "LAL.html"});
+});
+
+
